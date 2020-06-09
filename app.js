214b9abe80cb52a6d63b6264c39ba09e0d192076
mofe64 <ogunbiyioladapo33@gmail.com>
2020-06-09 11:00:09 +0100

    validtion implementation

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require('express')
-const morgan = require('morgan'); //logger
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require("express");
+const morgan = require("morgan"); //logger
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config({ path: "./.env" });
 
 const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan('tiny'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("tiny"));
 
+const mailingRouter = require("./api/routes/mailingRoutes")();
 
-const mailingRouter = require('./api/routes/mailingRoutes')()
+app.use("/api/v1", mailingRouter);
 
-app.use('/api/v1', mailingRouter);
-
-app.get('/', (req, res) => {
-  res.send('home')
+app.get("/", (req, res) => {
+  res.send("home");
 });
 
-port = 4000
+port = 4000;
 app.listen(port, function () {
-  console.log(`Listening on port ${port}...`)
-})
-
+  console.log(`Listening on port ${port}...`);
+});
