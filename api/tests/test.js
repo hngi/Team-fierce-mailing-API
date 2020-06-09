1df3495b708fe0d0ce1cc52d470a189d8e922769
trustidkid <yemi.bili07@gmail.com>
2020-06-09 10:55:18 +0100

    feat: added mail validator

---
+++ api/tests/test.js
@@ -0,0 +1,7 @@
+function ValidateEmail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)) {
+        return (true)
+    }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
---
+++ api/utils/mail_validator.js
@@ -0,0 +1,12 @@
+function ValidateEmail(email) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(mailformat)) {
+        //change the input tag to focus
+        document.form.text.focus();
+        return true;
+    } else {
+        alert("You have entered an invalid email address!");
+        document.form.text.focus();
+        return false;
+    }
+}
