759584f3cf0eb20bb66b104d2d81ee59a288312a
Abraham <ekekhorabraham@gmail.com>
2020-06-12 17:44:56 +0100

    added new feature

--- api/utils/token_validator.js
+++ api/utils/token_validator.js
@@ -1,3 +1,16 @@
 exports.validateToken = (acc_id, token) => {
+    let sql = 'SELECT * FROM users WHERE account_id = ?';
+
+    connection.query(sql, [acc_id], function (error, results, fields) {
+        if (error) {
+            return false
+        }else{
+          if(results.length >0){
+              if(token == results[0].token){
+                    return true
+                }
+            }
+        }
+    });
     return false
 }
