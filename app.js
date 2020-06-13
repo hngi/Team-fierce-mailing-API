06fa23064d1e66b5ff2672fad23675b40aee8f52
Abduljafar Kamarudeen <abduljafar.kamaru@ekedp.com>
2020-06-13 16:33:01 +0100

    added docs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan'); //logger
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const path = require('path');
 require('dotenv').config()
 
 const app = express();
@@ -17,7 +18,11 @@ app.use('/v1/mail', mailingRouter);
 app.use('/v1/user', userRouter);
 
 app.get('/', (req, res) => {
-  res.send('home')
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/v1/documentation', (req, res) => {
+  res.sendFile(path.join(__dirname+'/doc.json'));
 });
 
 port = process.env.PORT
